efcd972a3dc670abbdfe0253ef36dcf7d25fa6ad
bacebu4 <vasua14735@icloud.com>
2020-10-29 19:45:46 +0300

    log in page created

--- src/pages/auth/AuthLoginScreen.js
+++ src/pages/auth/AuthLoginScreen.js
@@ -1,24 +1,42 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { StyleSheet, View, Text, TextInput } from "react-native";
 import { MainButton } from "../../MainButton";
-import { MainLink } from "../../MainLink";
 import { NavbarTop } from "../../components/NavbarTop";
 
 export const AuthLoginScreen = ({ navigation }) => {
+  const [email, onEmail] = React.useState("some@some.com");
+  const [password, onPassword] = React.useState("123456");
+
+  const handleSubmit = () => {
+    console.log("email: ", email);
+    console.log("password: ", password);
+  };
   return (
-    <>
-      <View style={styles.container}>
-        <NavbarTop
-          title="Log In"
-          handleClick={() => navigation.goBack()}
-        ></NavbarTop>
-        <View style={styles.center}>
-          <View style={styles.button}>
-            <MainButton title="Log In"></MainButton>
-          </View>
+    <View style={styles.container}>
+      <NavbarTop
+        title="Log In"
+        handleClick={() => navigation.goBack()}
+      ></NavbarTop>
+      <Text style={styles.heading}>Email</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => onEmail(text)}
+        value={email}
+        keyboardType="email-address"
+      />
+      <Text style={styles.heading}>Password</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => onPassword(text)}
+        value={password}
+        secureTextEntry
+      />
+      <View style={styles.center}>
+        <View style={styles.button}>
+          <MainButton title="Log In" clickAction={handleSubmit}></MainButton>
         </View>
       </View>
-    </>
+    </View>
   );
 };
 
@@ -28,6 +46,21 @@ const styles = StyleSheet.create({
     paddingRight: 32,
     flex: 1,
     backgroundColor: "white",
+  },
+  heading: {
+    fontSize: 18,
+    color: "#343434",
+    fontWeight: "700",
+    marginTop: 22,
+  },
+  input: {
+    height: 40,
+    backgroundColor: "#e5e5e5",
+    borderRadius: 4,
+    color: "#343434",
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    marginTop: 8,
   },
   image: {
     width: 315,
