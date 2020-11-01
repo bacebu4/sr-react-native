87093819bd0c01e34aea794b1b280c0f65136e03
bacebu4 <vasua14735@icloud.com>
2020-11-01 19:35:34 +0300

    getting initial info

--- src/store/NotesStore.js
+++ src/store/NotesStore.js
@@ -4,22 +4,28 @@ import request from "../functions/request";
 
 class NotesStore {
   highlights = [];
+  tags = [];
+  latestBooks = [];
   amount = 3;
   email = null;
 
   constructor() {
     makeObservable(this, {
       highlights: observable,
+      tags: observable,
+      latestBooks: observable,
       amount: observable,
       fetchHighlights: flow,
       init: flow,
       setEmail: action,
+      setTags: action,
+      setLatestBooks: action,
+      setAmount: action,
     });
   }
 
   *fetchHighlights() {
     try {
-      console.log(this.email);
       const notes = yield request(
         `http://192.168.1.70:3000/api/getDailyNotes?id=1`
       );
@@ -32,6 +38,12 @@ class NotesStore {
   *init(email) {
     try {
       this.setEmail(email);
+      const initInfo = yield request(
+        `http://192.168.1.70:3000/api/getInitInfo?id=1`
+      );
+      this.setTags(initInfo.tags);
+      this.setLatestBooks(initInfo.latestBooks);
+      this.setAmount(initInfo.accountInfo.review_amount);
     } catch (error) {
       console.log(error);
     }
@@ -39,6 +51,18 @@ class NotesStore {
 
   setEmail(value) {
     this.email = value;
+  }
+
+  setTags(value) {
+    this.tags = value;
+  }
+
+  setLatestBooks(value) {
+    this.latestBooks = value;
+  }
+
+  setAmount(value) {
+    this.amount = value;
   }
 }
 
