82c2a1c9107d84cc327fe9a927ada0bfbb2aa96b
bacebu4 <vasua14735@icloud.com>
2020-11-07 15:23:33 +0300

    search bar added

--- src/pages/SearchScreen.js
+++ src/pages/SearchScreen.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, ScrollView } from "react-native";
 import { Container } from "../components/grid/Container";
 import { MainContainer } from "../components/grid/MainContainer";
 import Constants from "expo-constants";
 import { Title } from "../Title";
+import { SearchBar } from "react-native-elements";
 
 export const SearchScreen = () => {
+  const [search, setSearch] = useState("");
+
   return (
     <MainContainer>
       <ScrollView>
         <Container mt={Constants.statusBarHeight + 40}>
           <Title type="big" title="Search" />
+          <SearchBar
+            placeholder="Highlights"
+            onChangeText={setSearch}
+            value={search}
+            platform="ios"
+            inputContainerStyle={{
+              height: 35,
+              marginLeft: 0,
+              backgroundColor: "#eee",
+            }}
+          />
         </Container>
       </ScrollView>
     </MainContainer>
