bd45b2885e02fd2db10001aff251ef2a5fd35934
bacebu4 <vasua14735@icloud.com>
2020-11-16 06:48:47 +0300

    using hook for convinience

---
+++ src/hooks/request.hook.js
@@ -0,0 +1,42 @@
+import { useState, useCallback } from "react";
+
+export const useRequest = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const request = useCallback(
+    async (url, method = "GET", token, data = null) => {
+      setLoading(true);
+      try {
+        const headers = {};
+        if (token) {
+          headers["auth-token"] = token;
+        }
+        let body = null;
+
+        if (data) {
+          headers["Content-Type"] = "application/json";
+          body = JSON.stringify(data);
+        }
+
+        const response = await fetch(url, {
+          method,
+          headers,
+          body,
+        });
+
+        return await response.json();
+      } catch (error) {
+        setError(e.message);
+        throw error;
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
+
+  const clearError = useCallback(() => setError(null), []);
+
+  return { loading, request, error, clearError };
+};
--- src/pages/ByBookScreen.js
+++ src/pages/ByBookScreen.js
@@ -1,23 +1,42 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState, useCallback } from "react";
 import { observer } from "mobx-react-lite";
 import { NotesStoreContext } from "../store/NotesStore";
 import { MainContainer } from "../components/grid/MainContainer";
 import { Container } from "../components/grid/Container";
 import { Card } from "../Card";
-import { FlatList, TouchableOpacity } from "react-native";
+import { Alert, FlatList, TouchableOpacity } from "react-native";
 import { ActivityIndicator } from "react-native";
+import { useRequest } from "../hooks/request.hook";
 
 export const ByBookScreen = observer(({ route }) => {
   const NotesStore = useContext(NotesStoreContext);
   const { book_id } = route.params;
+  const { request, loading } = useRequest();
+  const [notes, setNotes] = useState([]);
+
+  const fetchNotes = useCallback(async () => {
+    try {
+      const fetched = await request(
+        `http://192.168.1.70:3000/api/getNotesByBook`,
+        "POST",
+        NotesStore.token,
+        { book_id }
+      );
+      setNotes(fetched);
+      console.log(notes);
+    } catch (error) {
+      Alert.alert(error.message);
+    }
+  }, []);
 
   useEffect(() => {
-    NotesStore.getNotesByBook(book_id);
+    // NotesStore.getNotesByBook(book_id);
+    fetchNotes();
   }, []);
 
   return (
     <MainContainer>
-      {NotesStore.notesByBookLoading ? (
+      {loading ? (
         <>
           <Container center centerY style={{ flex: 1 }}>
             <ActivityIndicator size="large"></ActivityIndicator>
@@ -26,7 +45,7 @@ export const ByBookScreen = observer(({ route }) => {
       ) : (
         <>
           <FlatList
-            data={NotesStore.notesByBook}
+            data={notes}
             keyExtractor={(item) => item.note_id}
             renderItem={({ item }) => (
               <Container mt={16} mb={16} key={item.note_id}>
