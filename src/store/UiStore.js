89523dbc81e06b578d36dd289f93e2a38bf03d98
bacebu4 <vasua14735@icloud.com>
2020-11-24 08:02:04 +0300

    unused

--- src/store/UiStore.js
+++ src/store/UiStore.js
@@ -2,42 +2,8 @@ import { makeAutoObservable } from "mobx";
 import { createContext } from "react";
 
 class UiStore {
-  addNavigationRef = null;
-  addRef = null;
-  editRef = null;
-  showAddSheet = false;
-  showChooseSheet = false;
   currentNote = 0;
   currentTag = null;
-  showEditTagSheet = false;
-
-  setAddRef(value) {
-    this.addRef = value;
-  }
-
-  setEditRef(value) {
-    this.editRef = value;
-  }
-
-  setShowAddSheet(value) {
-    this.showAddSheet = value;
-  }
-
-  setShowEditSheet(value, tagId = null) {
-    this.showEditTagSheet = value;
-    if (this.editRef) {
-      if (value) {
-        this.editRef.current.snapTo(0);
-        this.currentTag = tagId;
-      } else {
-        this.editRef.current.snapTo(1);
-      }
-    }
-  }
-
-  setShowChooseSheet(value) {
-    this.showChooseSheet = value;
-  }
 
   setCurrentNote(value) {
     this.currentNote = value;
