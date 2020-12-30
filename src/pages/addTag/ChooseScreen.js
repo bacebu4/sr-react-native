bd099711223bcc6e8f03ccaece72a04e368f85b1
bacebu4 <vasua14735@icloud.com>
2020-12-30 13:28:38 +0300

    cleaner

--- src/pages/addTag/ChooseScreen.js
+++ src/pages/addTag/ChooseScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from "react";
-import { StyleSheet, View, Image } from "react-native";
+import { View, Image } from "react-native";
 import { Tag } from "../../Tag";
 import { observer } from "mobx-react-lite";
 import { NotesStoreContext } from "../../store/NotesStore";
@@ -33,76 +33,65 @@ export const ChooseScreen = observer(({ handleCancel, note }) => {
         height: 650,
       }}
     >
-      <>
-        {showAddSheet ? (
-          <>
-            <Container>
-              <NavbarTop
-                handleClick={handleCancel}
-                handleNext={handleShowCreate}
-                title="Choose from existing"
-                titleLeft="Cancel"
-                titleRight="Create"
-                hasNoMargin
-              ></NavbarTop>
-            </Container>
-            <Container hasBorder mt={16}></Container>
+      {showAddSheet ? (
+        <>
+          <Container>
+            <NavbarTop
+              handleClick={handleCancel}
+              handleNext={handleShowCreate}
+              title="Choose from existing"
+              titleLeft="Cancel"
+              titleRight="Create"
+              hasNoMargin
+            />
+          </Container>
+          <Container hasBorder mt={16} />
 
-            {NotesStore.tags.length ? (
-              <>
-                <Container>
-                  <TagContainer>
-                    {/* TODO getter for note tags */}
-                    {NotesStore.tags.map((tag) => {
-                      const findResults = note.tags.find(
-                        (t) => t.tag_id === tag.tag_id
+          {NotesStore.tags.length ? (
+            <>
+              <Container>
+                <TagContainer>
+                  {/* TODO getter for note tags */}
+                  {NotesStore.tags.map((tag) => {
+                    const findResults = note.tags.find(
+                      (t) => t.tag_id === tag.tag_id
+                    );
+
+                    if (!findResults) {
+                      return (
+                        <Tag
+                          title={tag.tag_name}
+                          hue={tag.hue}
+                          key={tag.tag_id}
+                          style={{ marginRight: 16, marginTop: 24 }}
+                          clickAction={() =>
+                            handleSubmitFromExisting(tag.tag_id)
+                          }
+                        />
                       );
-
-                      if (!findResults) {
-                        return (
-                          <Tag
-                            title={tag.tag_name}
-                            hue={tag.hue}
-                            key={tag.tag_id}
-                            style={{ marginRight: 16, marginTop: 24 }}
-                            clickAction={() =>
-                              handleSubmitFromExisting(tag.tag_id)
-                            }
-                          ></Tag>
-                        );
-                      }
-                    })}
-                  </TagContainer>
-                </Container>
-              </>
-            ) : (
-              // empty state
-              <>
-                <Container isCentered mt={44}>
-                  <Image
-                    style={styles.image}
-                    source={require("../../assets/empty_tags.png")}
-                  ></Image>
-                  <TextGray mt={16}>No tags created yet</TextGray>
-                </Container>
-              </>
-            )}
-          </>
-        ) : (
-          // adding new tag
-          <TagConstructor
-            handleBack={handleBack}
-            handleClose={handleCancel}
-          ></TagConstructor>
-        )}
-      </>
+                    }
+                  })}
+                </TagContainer>
+              </Container>
+            </>
+          ) : (
+            // empty state
+            <Container isCentered mt={44}>
+              <Image
+                style={{
+                  width: 153,
+                  height: 120,
+                }}
+                source={require("../../assets/empty_tags.png")}
+              />
+              <TextGray mt={16}>No tags created yet</TextGray>
+            </Container>
+          )}
+        </>
+      ) : (
+        // adding new tag
+        <TagConstructor handleBack={handleBack} handleClose={handleCancel} />
+      )}
     </View>
   );
-});
-
-const styles = StyleSheet.create({
-  image: {
-    width: 153,
-    height: 120,
-  },
 });
