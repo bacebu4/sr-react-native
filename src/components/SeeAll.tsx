3d9de5d7032152804d1d8b0880f5af4c70328e1b
bacebu4 <vasua14735@icloud.com>
2021-01-21 13:28:42 +0300

    using baseText

--- src/components/SeeAll.tsx
+++ src/components/SeeAll.tsx
@@ -1,29 +1,19 @@
 import React from "react";
-import { TText } from "./TText";
 import { BaseImage } from "./BaseImage";
-import {
-  StyleSheet,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
+import { BaseText } from "./BaseText";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 
 export const SeeAll: React.FC<TouchableOpacityProps> = (props) => {
   return (
-    <TouchableOpacity {...props} style={styles.wrapper}>
-      <TText style={styles.title}>See All</TText>
+    <TouchableOpacity
+      {...props}
+      style={{
+        flexDirection: "row",
+        alignItems: "center",
+      }}
+    >
+      <BaseText isBold>See All</BaseText>
       <BaseImage w={24} h={24} ml={4} source={require("../assets/arrow.png")} />
     </TouchableOpacity>
   );
 };
-
-const styles = StyleSheet.create({
-  wrapper: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  title: {
-    color: "#343434",
-    fontSize: 16,
-    fontWeight: "600",
-  },
-});
