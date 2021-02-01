625cd01c6581f588395996a209fb201c6dccc2e4
bacebu4 <vasua14735@icloud.com>
2021-02-01 08:19:13 +0300

    added util function for dates

---
+++ src/utils/getWeekDaysArray.ts
@@ -0,0 +1,44 @@
+import { startOfWeek, addDays, getDate, getDay } from "date-fns";
+
+function getDayName(dayOfWeek: number): DayOfWeek {
+  switch (dayOfWeek) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tue";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thu";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+    default:
+      throw new Error("");
+  }
+}
+
+type DayOfWeek = "Sun" | "Mon" | "Tue" | "Wed" | "Thu" | "Fri" | "Sat";
+
+interface Day {
+  dayOfTheMonth: number;
+  name: DayOfWeek;
+}
+
+export function getWeekDaysArray(date: Date): Array<Day> {
+  const weekDatesArray: Array<Day> = [];
+  const startOfWeekDate = startOfWeek(date);
+
+  for (let i = 1; i < 8; ++i) {
+    const dayOfWeek = addDays(startOfWeekDate, i);
+    const dayOfWeekNumber = getDay(dayOfWeek);
+    const dayOfTheMonth = getDate(dayOfWeek);
+    const dayName = getDayName(dayOfWeekNumber);
+    weekDatesArray.push({ dayOfTheMonth, name: dayName });
+  }
+
+  return weekDatesArray;
+}
