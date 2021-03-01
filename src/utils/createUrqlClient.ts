508d8fcef563d713c358c111b6408a622959051c
bacebu4 <vasua14735@icloud.com>
2021-03-01 15:46:44 +0300

    refactor: made code safere without ! marks

--- src/utils/createUrqlClient.ts
+++ src/utils/createUrqlClient.ts
@@ -26,44 +26,50 @@ export const createUrqlClient = (TOKEN: string) => {
       cacheExchange({
         optimistic: {
           addComment: (variables, cache, _) => {
+            const { commentId, noteId, text } = variables;
+
             const cachedComments = cache.readQuery<NoteQuery>({
               query: NoteDocument,
-              variables: { id: variables.noteId },
-            })!.note!.comments;
+              variables: { id: noteId },
+            })?.note?.comments;
 
             const addedComment: Comment = {
-              id: variables.commentId as string,
-              noteId: variables.noteId as string,
-              text: variables.text as string,
+              id: commentId as string,
+              noteId: noteId as string,
+              text: text as string,
               createdAt: String(Date.now()),
               __typename: "Comment",
             };
 
-            return {
-              __typename: "Note",
-              id: variables.noteId,
-              comments: [...cachedComments, addedComment],
-            } as any;
+            if (cachedComments) {
+              return {
+                __typename: "Note",
+                id: noteId,
+                comments: [...cachedComments, addedComment],
+              } as any;
+            }
           },
           addExistingTag: (variables, cache, _) => {
+            const { noteId, tagId } = variables;
+
             const cachedTagsFromNote = cache.readQuery<NoteQuery>({
               query: NoteDocument,
-              variables: { id: variables.noteId },
-            })!.note!.tags;
+              variables: { id: noteId },
+            })?.note?.tags;
 
             const allCachedTags = cache.readQuery<TagsQuery>({
               query: TagsDocument,
-            })!.tags;
+            })?.tags;
 
-            const addedTag = allCachedTags?.find(
-              (t) => t?.id === variables.tagId
-            );
+            const addedTag = allCachedTags?.find((t) => t?.id === tagId);
 
-            return {
-              __typename: "Note",
-              id: variables.noteId,
-              tags: [...cachedTagsFromNote!, addedTag],
-            } as any;
+            if (cachedTagsFromNote) {
+              return {
+                __typename: "Note",
+                id: noteId,
+                tags: [...cachedTagsFromNote, addedTag],
+              } as any;
+            }
           },
           updateTag: (variables, cache, _) => {
             const { name, hue, tagId } = variables;
@@ -71,19 +77,21 @@ export const createUrqlClient = (TOKEN: string) => {
             const allFields = cache.inspectFields("Query");
             const noteQueries = allFields.filter((x) => x.fieldName === "note");
             noteQueries.forEach((q) => {
-              const noteId = q!.arguments!.id;
-              cache.updateQuery<NoteQuery>(
-                { query: NoteDocument, variables: { id: noteId } },
-                (data) => {
-                  data?.note?.tags?.forEach((t) => {
-                    if (t?.id === tagId) {
-                      t.name = name as string;
-                      t.hue = hue as number;
-                    }
-                  });
-                  return data;
-                }
-              );
+              const noteId = q?.arguments?.id;
+              if (noteId) {
+                cache.updateQuery<NoteQuery>(
+                  { query: NoteDocument, variables: { id: noteId } },
+                  (data) => {
+                    data?.note?.tags?.forEach((t) => {
+                      if (t?.id === tagId) {
+                        t.name = name as string;
+                        t.hue = hue as number;
+                      }
+                    });
+                    return data;
+                  }
+                );
+              }
             });
 
             return null;
@@ -128,37 +136,37 @@ export const createUrqlClient = (TOKEN: string) => {
             if (cachedTagsFromNote) {
               return {
                 __typename: "Note",
-                id: variables.noteId,
+                id: noteId,
                 tags: [...cachedTagsFromNote, addedTag],
               } as any;
             }
           },
           deleteTagFromNote: (variables, cache, _) => {
+            const { noteId, tagId } = variables;
+
             const cachedTagsFromNote = cache.readQuery<NoteQuery>({
               query: NoteDocument,
-              variables: { id: variables.noteId },
+              variables: { id: noteId },
             })?.note?.tags;
 
             return {
               __typename: "Note",
-              id: variables.noteId,
-              tags: cachedTagsFromNote?.filter(
-                (t) => t?.id !== variables.tagId
-              ),
+              id: noteId,
+              tags: cachedTagsFromNote?.filter((t) => t?.id !== tagId),
             } as any;
           },
           deleteComment: (variables, cache, _) => {
+            const { noteId, commentId } = variables;
+
             const cachedComments = cache.readQuery<NoteQuery>({
               query: NoteDocument,
-              variables: { id: variables.noteId },
+              variables: { id: noteId },
             })?.note?.comments;
 
             return {
               __typename: "Note",
-              id: variables.noteId,
-              comments: cachedComments?.filter(
-                (c) => c?.id !== variables.commentId
-              ),
+              id: noteId,
+              comments: cachedComments?.filter((c) => c?.id !== commentId),
             } as any;
           },
           updateNote: (variables, _, __) => ({
@@ -175,7 +183,10 @@ export const createUrqlClient = (TOKEN: string) => {
             const { reviewAmount } = variables;
 
             cache.updateQuery<InfoQuery>({ query: InfoDocument }, (data) => {
-              data!.info!.reviewAmount = reviewAmount as number;
+              if (data?.info?.reviewAmount) {
+                data.info.reviewAmount = reviewAmount as number;
+              }
+
               return data;
             });
 
@@ -183,8 +194,13 @@ export const createUrqlClient = (TOKEN: string) => {
           },
           updateReviewHistory: (_, cache, __) => {
             cache.updateQuery<InfoQuery>({ query: InfoDocument }, (data) => {
-              data!.info!.streak = data!.info!.streak + 1;
-              data!.info!.reviewed = true;
+              if (data?.info?.streak) {
+                data.info.streak = data.info.streak + 1;
+              }
+
+              if (data?.info?.reviewed) {
+                data.info.reviewed = true;
+              }
               return data;
             });
 
@@ -224,19 +240,22 @@ export const createUrqlClient = (TOKEN: string) => {
                 (x) => x.fieldName === "note"
               );
               noteQueries.forEach((q) => {
-                const noteId = q!.arguments!.id;
-                cache.updateQuery<NoteQuery>(
-                  { query: NoteDocument, variables: { id: noteId } },
-                  (data) => {
-                    data?.note?.tags?.forEach((t) => {
-                      if (t?.id === tagId) {
-                        t.name = name as string;
-                        t.hue = hue as number;
-                      }
-                    });
-                    return data;
-                  }
-                );
+                const noteId = q?.arguments?.id;
+
+                if (noteId) {
+                  cache.updateQuery<NoteQuery>(
+                    { query: NoteDocument, variables: { id: noteId } },
+                    (data) => {
+                      data?.note?.tags?.forEach((t) => {
+                        if (t?.id === tagId) {
+                          t.name = name as string;
+                          t.hue = hue as number;
+                        }
+                      });
+                      return data;
+                    }
+                  );
+                }
               });
             },
             deleteTag: (_, args, cache, __) => {
